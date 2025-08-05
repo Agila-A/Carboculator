216d07b669fbcdfea68a4fe173bf73aadd27eb19
dhanesh <dhaneshprabhupro@gmail.com>
2025-08-05 19:43:13 +0530

    Dhanesh

--- carboculator-frontend/src/Components/Users/DashBoard/PlantTree.jsx
+++ carboculator-frontend/src/Components/Users/DashBoard/PlantTree.jsx
@@ -2,6 +2,7 @@ import tree from "../../../assets/tree.png"
 import leaf from "../../../assets/leaf.png"
 import leaves from "../../../assets/leaves.png"
 import leaf2 from "../../../assets/leaf2.png"
+import { TextField, MenuItem } from '@mui/material';
 
 
 const PlantTree = () => {
@@ -13,22 +14,30 @@ const PlantTree = () => {
                 <img src={leaf} width="30px" height="30px" style={{marginLeft:"7%",marginBottom:0,overflow:"hidden"}}/>
                 <h1 style={{fontSize:"60px",color:"rgba(67, 99, 58, 1)",marginLeft:"7%",marginBottom:0,marginTop:0,fontWeight:"bold",}}>Plant A Tree For</h1>
                 <h1 style={{fontSize:"60px",color:"rgba(67, 99, 58, 1)",marginLeft:"7%",marginBottom:0,marginTop:0,fontWeight:"bold",}}>Better Life</h1>
-                <p style={{width:"90%",marginLeft:"7%",marginTop:0,padding:"2% 0% 0% 0%"}}>The Plant a Tree feature helps users calculate the number of trees needed to offset their carbon emissions. an actionable tree-planting goal. It's an easy way to promote sustainability and achieve carbon neutrality.</p>
+                <p style={{width:"90%" ,marginLeft:"7%",marginTop:0,padding:"2% 0% 0% 0%"}}>The Plant a Tree feature helps users calculate the number of trees needed to offset their carbon emissions. an actionable tree-planting goal. It's an easy way to promote sustainability and achieve carbon neutrality.</p>
                 <div style={{width:"65%",margin:0,padding:0}}>
                     <img src={leaf2} width="30px" height="30px" style={{marginLeft:"100%",marginBottom:0}}/>
                 </div>
                 <div style={{display:"flex",flexDirection:"column",width:"65%",border:"1px solid ",marginLeft:"7%",padding:"1%",marginTop:0,borderRadius:"5px"}}>
                     <div style={{display:"flex",width:"94%",padding:"3%"}}>
                         <div style={{width:"35%"}}>Area of Land Required</div>
-                        <div style={{width:"70%",height:"50%",border:"1px solid grey",borderRadius:"5px",textAlign:"center"}}>0</div>
+                        <div style={{width:"70%",height:"79%",border:"1px solid grey",borderRadius:"5px",display:"flex",justifyContent:"center",alignItems:"center"}}>0</div>
                     </div>
                     <div style={{display:"flex",width:"94%",padding:"3%"}}>
                         <div style={{width:"35%"}}>No of trees to be planted</div>
-                        <div style={{width:"70%",height:"50%",border:"1px solid grey",borderRadius:"5px",textAlign:"center"}}>0</div>
+                        <div style={{width:"70%",height:"79%",border:"1px solid grey",borderRadius:"5px",display:"flex",justifyContent:"center",alignItems:"center"}}>0</div>
                     </div>
                     <div style={{display:"flex",width:"94%",padding:"3%"}}>
                         <div style={{width:"35%"}}>Species of the tree</div>
-                        <div style={{width:"70%",height:"50%",border:"1px solid grey",borderRadius:"5px",textAlign:"center"}}>0</div>
+                        <div style={{width:"70%",height:"60%",textAlign:"center"}}>
+                        <TextField select size="small" style={{width:"100%"}}  required>
+                            <MenuItem value="Neem">Neem</MenuItem>
+                            <MenuItem value="Peepal">Peepal</MenuItem>
+                            <MenuItem value="Banyan">Banyan</MenuItem>
+                            <MenuItem value="Mango">Mango</MenuItem>
+                        </TextField>
+                        
+                        </div>
                     </div>
                 </div>
 
